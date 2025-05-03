889af7354f64bba187f2b6805a339468e61e2e01
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2025-05-04 00:07:00 +0200

    Add brightness and mode rendering to scene Rendering is to Render scene hook so that it still draws in main menu and camera

--- lua/postprocess/init.lua
+++ lua/postprocess/init.lua
@@ -1,3 +1,6 @@
+--#TODO: Dubois algorithm to help against ghosting and color accuracy issues
+
+
 -- Toggle convar
 local pp_anaglyph_3d = CreateClientConVar("pp_anaglyph_3d", "0", false, false)
 local pp_anaglyph_3d_eye_separation = CreateClientConVar("pp_anaglyph_3d_eye_separation", "63", true, false, "millimeters (63 is average for adults and in the range of 50-75)", 0, 100)
@@ -7,7 +10,8 @@ local pp_anaglyph_3d_draw_monitors = CreateClientConVar("pp_anaglyph_3d_draw_mon
 local pp_anaglyph_3d_use_postprocess = CreateClientConVar("pp_anaglyph_3d_use_postprocess", "1", true, false)
 local pp_anaglyph_3d_fov = CreateClientConVar("pp_anaglyph_3d_fov", "0", true, false, "FOV for the anaglyph 3D effect. 0 = default FOV", 0, 150)
 local pp_anaglyph_3d_crosshair = CreateClientConVar("pp_anaglyph_3d_crosshair", "0", true, false)
-local pp_anaglyph_3d_crosseyedness = CreateClientConVar("pp_anaglyph_3d_crosseyedness", "0.6", true, false, "Crosseyedness in degrees (0.5 is 5 degree)", 0, 5)
+local pp_anaglyph_3d_crosseyedness = CreateClientConVar("pp_anaglyph_3d_crosseyedness", "0.6", true, false, "Crosseyedness in degrees", 0, 5)
+local pp_anaglyph_3d_brightness = CreateClientConVar("pp_anaglyph_3d_brightness", "1", true, false, "Brightness of the anaglyph 3D effect. 0 = blackscreen, 1 = normal brightness, 2 = double brightness", 0, 10)
 
 
 -- Create RTs & materials
@@ -78,26 +82,27 @@ hook.Add("RenderScene", "Anaglyph3D_Capture", function(origin, angles, fov)
     render.RenderView(view)
     render.PopRenderTarget()
 
+    -- if not pp_anaglyph_3d:GetBool() then return end
+
+    matLeft:SetVector("$color", Vector(pp_anaglyph_3d_brightness:GetFloat(), 0, 0)) -- Only store red
+    matRight:SetVector("$color", Vector(0, pp_anaglyph_3d_brightness:GetFloat(), pp_anaglyph_3d_brightness:GetFloat())) -- Only store green and blue (cyan)
+    cam.Start2D()
+    
+        render.OverrideBlend(true, BLEND_ONE, BLEND_ONE, BLENDFUNC_ADD)
+        
+        surface.SetDrawColor(255,255,255,255)   -- full brightness, material handles channel mask
+        
+        surface.SetMaterial(matLeft)
+        surface.DrawTexturedRect(0,0,ScrW(),ScrH())
+        
+        surface.SetMaterial(matRight)
+        surface.DrawTexturedRect(0,0,ScrW(),ScrH())
+        render.OverrideBlend(false)
+
+    cam.End2D()
+    
     
     return true  -- fully override default scene 
-end)
-
-
-hook.Add("DrawOverlay", "Anaglyph3D_Composite", function()
-    if not pp_anaglyph_3d:GetBool() then return end
-
-    render.OverrideBlend(true, BLEND_ONE, BLEND_ONE, BLENDFUNC_ADD)
-
-    surface.SetDrawColor(255,255,255,255)   -- full brightness, material handles channel mask
-
-    surface.SetMaterial(matLeft)
-    surface.DrawTexturedRect(0,0,ScrW(),ScrH())
-    
-    surface.SetMaterial(matRight)
-    surface.DrawTexturedRect(0,0,ScrW(),ScrH())
-    render.OverrideBlend(false)
-
-    return false
 end)
 
 hook.Add("HUDShouldDraw", "HideCrosshair", function(name)
@@ -127,21 +132,24 @@ list.Set( "PostProcess", "Anaglyph 3D", {
 
         form:NumSlider("Crosseyedness (degrees)", "pp_anaglyph_3d_crosseyedness", 0, 5, 2)
         form:ControlHelp("This sets the crosseyedness in degrees. This is the angle at which the eyes are turned inward to focus on an object, causing distant objects to look distant. A higher value will make the effect more pronounced, but may cause discomfort. Recommended to keep it between 0 and 1.5 degrees.")
-
-        form:CheckBox("No Viewmodel", "pp_anaglyph_3d_no_draw_viewmodel")
-        form:ControlHelp("This will draw the viewmodel in anaglyph 3D.")
-
+        
         -- form:CheckBox("Draw HUD", "pp_anaglyph_3d_draw_hud")
         -- form:ControlHelp("This will draw the HUD in anaglyph 3D.")
 
+        form:NumSlider("Brightness", "pp_anaglyph_3d_brightness", 0, 10, 1)
+        form:ControlHelp("This sets the brightness of the anaglyph 3D effect. Because some maps have a dark tint. 0 = blackscreen, 1 = normal brightness, 2 = double brightness.")
+        
         form:CheckBox("Draw Monitors", "pp_anaglyph_3d_draw_monitors")
         form:ControlHelp("This will draw monitors such as cameras and TVs while in anaglyph 3D.")
-
-        form:CheckBox("Use Post Process", "pp_anaglyph_3d_use_postprocess")
-        form:ControlHelp("This will use post-process effects to draw the anaglyph 3D effect.")
-
+        
         form:CheckBox("Crosshair", "pp_anaglyph_3d_crosshair")
         form:ControlHelp("This will draw the crosshair in anaglyph 3D mode.")
+        
+        form:CheckBox("Use Post Process", "pp_anaglyph_3d_use_postprocess")
+        form:ControlHelp("This will use post-process effects to draw the anaglyph 3D effect.")
+        
+        form:CheckBox("No Viewmodel", "pp_anaglyph_3d_no_draw_viewmodel")
+        form:ControlHelp("This will draw the viewmodel in anaglyph 3D.")
 
         form:NumSlider("Viewmodel FOV", "pp_anaglyph_3d_fov", 0, 150, 0)
         form:ControlHelp("Change the viewmodel FOV for the anaglyph 3D effect. Use 0 for default. A higher FOV moves the viewmodel farther from the camera, enhancing the 3D effect. If the viewmodel is too close, the red and cyan images may be too far apart for your eyes to merge comfortably.")
